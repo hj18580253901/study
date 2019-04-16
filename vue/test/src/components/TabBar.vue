79a4c2a5f0598bb19b5b08ee3905909117005869
hj18580253901 <1083298058@qq.com>
2019-04-16 10:33:18 +0800

    解决tablebar fixed抖动的问题

--- vue/test/src/components/TabBar.vue
+++ vue/test/src/components/TabBar.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="tabbar_box">
-    <router-link class="item" to="/index"><i class="iconfont icon-shouye1"></i><span>首页</span></router-link>
-    <router-link class="item" to="/vicinity"><i class="iconfont icon-fujinhui"></i> <span>附近</span></router-link>
-    <router-link class="item" to="/find"><i class="iconfont icon-n5appqz"></i><span>发现</span></router-link>
-    <router-link class="item" to="/order"><i class="iconfont icon-dingdan-l"></i><span>订单</span> </router-link>
-     <router-link class="item" to="/owner"><i class="iconfont icon-wode"></i><span>我的</span> </router-link>
+    <div class="tabBarMain">    
+      <router-link class="item" to="/index"><i class="iconfont icon-shouye1"></i><span>首页</span></router-link>
+      <router-link class="item" to="/vicinity"><i class="iconfont icon-fujinhui"></i> <span>附近</span></router-link>
+      <router-link class="item" to="/find"><i class="iconfont icon-n5appqz"></i><span>发现</span></router-link>
+      <router-link class="item" to="/order"><i class="iconfont icon-dingdan-l"></i><span>订单</span> </router-link>
+      <router-link class="item" to="/owner"><i class="iconfont icon-wode"></i><span>我的</span> </router-link>
+    </div>
   </div>
 </template>
 <script>
@@ -16,15 +18,22 @@ export default {
   @import '~@src/assets/style/font.scss' ;
   .tabbar_box{
     z-index:1111;
-    display:flex;
-    justify-content:space-around;
     background:#ebe8e8;
     position:fixed;
     bottom:0;
     left:0;
-    width:100vw;
+    transform: translateZ(0);
+    -webkit-transform: translateZ(0);
+    -moz-transform: translateZ(0);
+    width:100%;
     height:60px;
     border:1px slid red;
+    .tabBarMain{
+      width:100%;
+      height:60px;
+      display:flex;
+      justify-content:space-around;
+    }
     .item{
       padding:9px 15px;
       display:inline-block;
